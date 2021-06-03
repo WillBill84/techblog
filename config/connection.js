440bb3d0ff96ed22289965b227fb09b871afafc0
Will Locket <78286026+WillBill84@users.noreply.github.com>
2021-06-03 16:20:33 +1000

    host update

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
